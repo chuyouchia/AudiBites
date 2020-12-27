e188114a6480ba62211936694cde8abe274ee505
chuyouchia <58682208+chuyouchia@users.noreply.github.com>
2020-12-27 15:41:27 +0800

    Add Multipart api call and Endpoints Framework

--- app/src/main/java/com/example/audibites/PDF2ImageActivity.kt
+++ app/src/main/java/com/example/audibites/PDF2ImageActivity.kt
@@ -2,24 +2,22 @@ package com.example.audibites;
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.pdf.PdfRenderer
 import android.net.Uri
-import android.os.*
+import android.os.Build
+import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import java.io.File
-import java.io.FileOutputStream
-import java.io.InputStream
-import java.io.OutputStream
-import java.text.SimpleDateFormat
-import java.util.*
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+
 
 class PDF2ImageActivity : AppCompatActivity() {
-
+    private var mStorageRef: StorageReference? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pdf2image)
@@ -43,11 +41,12 @@ class PDF2ImageActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+        mStorageRef = FirebaseStorage.getInstance().reference;
 
     }
 
     private var selectedPDFUri: Uri? = null
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
@@ -55,86 +54,35 @@ class PDF2ImageActivity : AppCompatActivity() {
             //proceed to select the pdf file
             selectedPDFUri = data.data
 
-            //process the pdf into a series of bitmaps
-            load(selectedPDFUri!!)
+            //send the pdf file to the server to be processed
+            sendToAppServer(selectedPDFUri!!)
 
-        }
-    }
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    private fun load(uri: Uri){
-        //convert uri to file object
-        /*val `in`: InputStream? = contentResolver.openInputStream(uri)
-        val documentFileOutputStream: OutputStream = FileOutputStream(File(uri.path))
-        val buf = ByteArray(1024)
-        var len: Int
-        while (`in`!!.read(buf).also { len = it } > 0) {
-            documentFileOutputStream.write(buf, 0, len)
-        }
-        documentFileOutputStream.close()
-        `in`.close()
-         */
-
-        // Create the page renderer for the PDF document.
-        val fileDescriptor = contentResolver.openFileDescriptor(uri, "r")
-        val pdfRenderer = PdfRenderer(fileDescriptor!!)
-
-        for (i in 0 until pdfRenderer.pageCount) {
-
-            val page = pdfRenderer.openPage(i)
-            Log.d("PDF renderer", "current page saving is: ${i.toString()}")
-            // Render the page to the bitmap.
-            val bitmap = Bitmap.createBitmap(page.width, page.height, Bitmap.Config.ARGB_8888)
-            saveTempBitmap(bitmap)
-
-            // close the page
-            page.close()
-        }
-
-        // Close the `PdfRenderer` when you are done with it.
-        pdfRenderer.close()
-    }
-
-    private fun saveTempBitmap(bitmap: Bitmap) {
-        if (isExternalStorageWritable()) {
-            saveImage(bitmap)
-        } else {
-            //prompt the user or do something by displaying error message
-            Toast.makeText(this, "Unable to save", Toast.LENGTH_SHORT)
+            //get back the voice file
+            getAudioFromBackend(selectedPDFUri!!)
         }
     }
 
-    private fun saveImage(finalBitmap: Bitmap) {
-        val root: String = Environment.getExternalStorageState()
-        val myDir = File("$root/audibites/saved_images")
-        myDir.mkdirs()
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val fname = "Audibites_$timeStamp.jpg"
-        val file = File(myDir, fname)
+    private fun getAudioFromBackend(uri: Uri) {
+        //get the python backend to process the file via api call
 
-
-        if (!file.exists()) {
-            file.createNewFile();
-        } else {
-            file.delete()
-        }
-        try {
-            val out = FileOutputStream(file)
-            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
-            out.flush()
-            out.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
+        //get the processed file via simple download //todo change to streamed download
     }
 
-    /* Checks if external storage is available for read and write */
-    private fun isExternalStorageWritable(): Boolean {
-        val state: String = Environment.getExternalStorageState()
-        return Environment.MEDIA_MOUNTED == state
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun sendToAppServer(uri: Uri) {
+        //set the path of the file dynamically using its original file structure
+        val riversRef: StorageReference? = mStorageRef?.child(uri.path!!);
+
+        riversRef!!.putFile(uri)
+            .addOnSuccessListener(this,  OnSuccessListener<UploadTask.TaskSnapshot>() {
+                print("success!")
+                //TODO get the voice file
+            }).addOnFailureListener {
+                    // Handle unsuccessful uploads
+                    print("failed")
+            }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-    }
+
 
 }
